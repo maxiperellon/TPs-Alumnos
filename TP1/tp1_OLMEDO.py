73d25b0fd76c960f822cba844b781c502655ee21
Luchiolm76 <lucianoolmedofrancisco@gmail.com>
2024-03-19 12:56:39 -0300

    tp1 OLMEDO

---
+++ TP1/tp1_OLMEDO.py
@@ -0,0 +1,39 @@
+#PROFESOR: MAXIMILIANO PERELLÓN
+#ALUMNO: ....
+
+#           TRABAJO PRACTICO Nº1
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+
+#Ejercicio 1
+base= float(input("Ingrese la base: "))
+altura= float(input("Ingrese la altura: "))
+area= (base * altura)/2
+print("El valor del area es: ", area)
+#Ejercicio 2
+#codigo
+
+#Ejercicio 3
+#codigo
+
+#Ejercicio 4
+#codigo
+
+#Ejercicio 5
+#codigo
+
+#Ejercicio 6
+#codigo
+
+#Ejercicio 7
+#codigo
+
+#Ejercicio 8
+#codigo
+
+#Ejercicio 9
+#codigo

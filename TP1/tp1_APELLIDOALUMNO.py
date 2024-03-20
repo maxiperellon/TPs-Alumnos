1947939447f1da7995f119c24b51fa364f81b289
BigMaxi <maxigomez4425@gmail.com>
2024-03-20 10:17:51 -0300

    commit inciial

---
+++ TP1/tp1_APELLIDOALUMNO.py
@@ -0,0 +1,41 @@
+#PROFESOR: MAXIMILIANO PERELLÓN
+#ALUMNO: ....
+
+#           TRABAJO PRACTICO Nº1
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+
+#Ejercicio 1
+#
+# base = int(input("Ingrese la base del triangulo: "))
+# altura = int(input("Ingrese la altura del triangulo: "))
+# area = (base * altura) / 2
+# print("El area del triangulo es: ", area)
+
+#Ejercicio 2
+#
+
+#Ejercicio 3
+#codigo
+
+#Ejercicio 4
+#codigo
+
+#Ejercicio 5
+#codigo
+
+#Ejercicio 6
+#codigo
+
+#Ejercicio 7
+#codigo
+
+#Ejercicio 8
+#codigo
+
+#Ejercicio 9
+#codigo

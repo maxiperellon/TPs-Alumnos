5aded4cfd719ec502cfe16e2fe9d698db3a70efe
AugustoAlvarez <augustoalvarezcarp@gmail.com>
2024-03-20 09:45:42 -0300

    init commit

---
+++ tp1_APELLIDOALUMNO.py
@@ -0,0 +1,96 @@
+#PROFESOR: MAXIMILIANO PERELLÓN
+#ALUMNO: AuUGUSTO ALVAREZ 
+
+#           TRABAJO PRACTICO Nº1
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+
+#Ejercicio 1
+#codigo
+
+# b = float(input("Ingrese el valor de la base del "))
+# h = float(input("Ingrese el valor de la altura"))
+# a = (b*h)/2
+# print("el area del triangulo es", a)
+
+#Ejercicio 2
+#codigo
+
+# nombre = str(input("Ingrese su nombre"))
+# edad  = int(input("ingrese edad"))
+# curso = int(input("Ingrese el curso(numero/año)"))
+# carrera = str(input("Ingrese el nombre de su carrera"))
+# print("Mi nombre es", nombre, "tengo", edad, ",estoy en", curso, "de la carrera de", carrera)
+
+#Ejercicio 3
+#codigo
+
+# num1 = int(input("Ingrese un numero entero"))
+# num2 = int(input("Ingrese otro numero entero"))
+# print("La suma de los numeros es", num1+num2)
+
+
+#Ejercicio 4
+#codigo
+
+# radio =  float(input("ingrese el radio del circulo"))
+# pi =  3.14
+# a = (radio**pi)
+# print ("El Area del Circulo es", a)
+
+#Ejercicio 5
+#codigo
+
+# num1 = int(input("ingrese el primer numero entero"))
+# num2 = int(input("ingrese el segundo numero  entero"))
+# num3 = int(input("ingrese el tercer numero entero"))
+# promedio =  (num1 + num2 + num3) / 3
+# print("el promedio de los numeros es", promedio)
+
+#Ejercicio 6
+#codigo
+
+# num1 = int(input("Por favor, introduce el primer número entero"))
+# num2 = int(input("Por favor, introduce el segundo número entero"))
+# print("los numeros ingresados son:", num1,"y", num2)
+# temporal = num1
+# num1 = num2 
+# num2 = temporal 
+# print("los numeros intercambiados son:", num1,"y", num2)
+
+
+
+
+#Ejercicio 7
+#codigo
+
+# num1 = int(input("Por favor, introduce el primer número entero"))
+# num2 = int(input("Por favor, introduce el segundo número entero"))
+# suma = num1 + num2
+# resta = num1 - num2
+# multiplicacion = num1 * num2
+# division = num1 / num2
+# print("La suma de ", num1 , " y ", num2 , "es: ", suma )
+# print("La resta de ", num1 , " y ", num2 , "es: ", resta )
+# print("La multiplicación de ", num1 , " por ", num2 , "es: ", multiplicacion )
+# print("La división de ", num1 , " entre ", num2 , "es: ", division )
+
+#Ejercicio 8
+#codigo
+# materia_favorita = str([input("Ingresa tus materias favoritas: ")])
+# print ("Las materias que me gustan son: ", materia_favorita)
+
+#Ejercicio 9
+#codigo
+
+# autos = ["Toyota", "Wolkswagen", "Peugeot", "Renault", "Ford", "Chevrolet", "Fiat", "Dodge"]
+# print(autos)
+# usuario_autos = int(input("Ingrese qué marca de autos desea ver por medio de números enteros: "))
+# elemento = autos[usuario_autos]
+# print("La información de la marca de autos que el usuario quiere ver es: ", elemento)
+
+

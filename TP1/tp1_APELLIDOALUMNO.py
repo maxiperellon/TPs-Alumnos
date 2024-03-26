c71bf2744e9e67836f5e3467765fdf23af5b410a
BigMaxi <maxigomez4425@gmail.com>
2024-03-26 11:42:13 -0300

    tp1_GomezMaximo

--- TP1/tp1_APELLIDOALUMNO.py
+++ TP1/tp1_APELLIDOALUMNO.py
@@ -1,41 +1,80 @@
-#PROFESOR: MAXIMILIANO PERELLÓN
-#ALUMNO: ....
+# #PROFESOR: MAXIMILIANO PERELLÓN
+# #ALUMNO: ....
 
-#           TRABAJO PRACTICO Nº1
+# #           TRABAJO PRACTICO Nº1
 
-#PARA COMENTAR UN BLOQUE DE CODIGO: 
-##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+# #PARA COMENTAR UN BLOQUE DE CODIGO: 
+# ##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
 
-#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
-##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+# #PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+# ##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
 
-#Ejercicio 1
-#
-# base = int(input("Ingrese la base del triangulo: "))
-# altura = int(input("Ingrese la altura del triangulo: "))
-# area = (base * altura) / 2
-# print("El area del triangulo es: ", area)
+# #Ejercicio 1
+# #
+# # base = int(input("Ingrese la base del triangulo: "))
+# # altura = int(input("Ingrese la altura del triangulo: "))
+# # area = (base * altura) / 2
+# # print("El area del triangulo es: ", area)
 
-#Ejercicio 2
-#
+# #Ejercicio 2
+# # nombre = input("Ingrese su nombre: ")
+# # edad = int(input("Ingrese su edad: "))
+# # curso = int(input("Ingrese en que curso esta: "))
+# # a = input("Ingrese su periodo si es primaria, secundaria o facultad: ")
+# # print(f"Mi nombre es {nombre} y tengo {edad} años. Estoy en {curso} año de {a}.")
 
-#Ejercicio 3
-#codigo
+# #Ejercicio 3
+# num1 = int(input("Ingrese el primer valor numerico "))
+# num2 = int(input("Ingrese el segundo valor numerico: "))
+# total = num1 + num2
+# print("El total de la suma es: ", total)
 
-#Ejercicio 4
-#codigo
+# #Ejercicio 4
 
-#Ejercicio 5
-#codigo
+# radio = float(input("Ingrese el valor del radio del circulo: "))
+# area = 3.14 * (radio ** 2)
+# print("El area del circulo es ", area)
+
+
+# #Ejercicio 5
+# num1 = int(input("Ingrese el primer valor: "))
+# num2 = int(input("Ingrese el segundo valor: "))
+# num3 = int(input("Ingrese el tercer numero "))
+# suma = (num1 + num2 + num3) 
+# promedio = (suma)/ 3
+# print("El promedio de los digitos ingresados es: ", promedio)
 
 #Ejercicio 6
-#codigo
+# num1 = int(input("Ingrese el primer valor: "))
+# num2 = int(input("Ingrese el segundo valor "))
+# num1, num2 = num2, num1
+# print("El primer valor es: ", num1)
+# print("El segundo valor es:: ", num2)
 
-#Ejercicio 7
-#codigo
+# # # #Ejercicio 7
+# num1 = int(input("Ingrese  el primer valor: "))
+# num2 = int(input("Ingrese  el segundo valor: "))
+# sum = num1 + num2
+# resta = num1 - num2
+# multi = num1 * num2
+# division = num1 / num2
 
-#Ejercicio 8
-#codigo
+# print("La suma de los valores es: ", sum, "La resta de los valores es: ", resta, "La multiplicación de los valores es: ", multi, "La división es: ", division)
 
-#Ejercicio 9
-#codigo
+# # # #Ejercicio 8
+# Lista = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+# print("lo que contiene la lista es: ", Lista)
+
+
+# # # # #Ejercicio 9
+# frutas = ["Banana", "Manzana", "Durazno", "Frutilla"]
+# print("los elementos de la lista son: ", frutas)
+# input=int(input("Ingrese un numero del 1 al 4 para ver un elemento"))
+# if input== 1:
+#     print ("La fruta es una Banana")
+# elif input==2:
+#     print ("La fruta es una Manzana")
+# elif input==3:
+#     print ("La fruta es un Durazno")
+# else:
+#     print("Frutilla")

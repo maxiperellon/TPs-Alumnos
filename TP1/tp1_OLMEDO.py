0fde298def9a1777bfe13db2d07541dd02494a9c
Luchiolm76 <lucianoolmedofrancisco@gmail.com>
2024-03-26 11:09:49 -0300

    tp1_OLMEDO

--- TP1/tp1_OLMEDO.py
+++ TP1/tp1_OLMEDO.py
@@ -10,30 +10,81 @@
 ##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
 
 #Ejercicio 1
-base= float(input("Ingrese la base: "))
-altura= float(input("Ingrese la altura: "))
-area= (base * altura)/2
-print("El valor del area es: ", area)
+#codigo
+#base= float(input("Ingrese la base: "))
+#altura= float(input("Ingrese la altura: "))
+#area= (base * altura)/2
+#print("El valor del area es: ", area)
+
 #Ejercicio 2
 #codigo
+
+#nombre = str(input("Ingrese su nombre: "))
+#edad = int(input("Ingrese su edad: "))
+#curso = str(input("Ingrese en que curso esta: "))
+#carrera = str(input("Ingrese en que carrera esta: "))
+#print("Mi nombre es: ",nombre,"Tengo: ",edad,"Estoy en: ",curso,"Pertenezo a la carrera: ",carrera)
 
 #Ejercicio 3
 #codigo
 
+#numero1= int(input("Ingrese un numero entero: "))
+#numero2= int(input("Ingrese un numero entero: "))
+#print("La suma de los dos numeros es:",numero1+numero2)
+
 #Ejercicio 4
 #codigo
+
+#radio = int(input("Ingrese el radio del circulo: "))
+#print("El area del circulo es",3,14**radio)
 
 #Ejercicio 5
 #codigo
 
+#numero1 = int(input("Ingrese un numero entero: "))
+#numero2 = int(input("Ingrese un numero entero: "))
+#numero3 = int(input("Ingrese un numero entero: "))
+#promedio = (numero1+numero2+numero3) / 3
+#print(promedio)
+
 #Ejercicio 6
 #codigo
+
+# num1= int(input("Ingrese un numero entero: "))
+# num2= int(input("Ingrese un numero entero: "))
+# print("Los numeros ingresados son: ",num1,num2)
+# temp = num1
+# num1 = num2
+# num2 = temp
+# print("Los numeros intercambiados son: ",num1,num2)
 
 #Ejercicio 7
 #codigo
 
+# num1= int(input("Ingrese un numero entero: "))
+# num2= int(input("Ingrese un numero entero: "))
+# suma = (num1+num2)
+# resta =(num1-num2)
+# multiplicacion = (num1*num2)
+# division = (num1+num2) /2
+# print(suma, resta, multiplicacion, division)
+
 #Ejercicio 8
 #codigo
 
+# materias_preferidas = str([input("Ingrese sus materias preferidas: ")])
+# print("Mis materias preferidas son: ",materias_preferidas)
+
 #Ejercicio 9
 #codigo
+
+# lista_de_frutas = ["manzana","banana","naranja"]
+# frutas = int(input("Ingrese un numero del 1 al 3: "))
+# if frutas == 1: 
+#     print(lista_de_frutas[frutas-1])
+# elif frutas == 2:
+#     print(lista_de_frutas[frutas-1])
+# elif frutas == 3:
+#     print(lista_de_frutas[frutas-1])
+# else:
+#     print("Numero no valido ingrese del 1 al 3")

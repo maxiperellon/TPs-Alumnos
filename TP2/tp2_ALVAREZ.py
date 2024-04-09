7b9e6b568ac9d45010a7f8dde34405d9b297ea2a
AugustoAlvarez <augustoalvarezcarp@gmail.com>
2024-04-09 12:38:21 -0300

    cambios

---
+++ TP2/tp2_ALVAREZ.py
@@ -0,0 +1,54 @@
+#PROFESOR: MAXIMILIANO PERELLÓN
+#ALUMNO: ....
+
+#           TRABAJO PRACTICO Nº2 - CONDICIONALES
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+
+#Ejercicio 1   
+#codigo
+
+# edad = int(input("ingrese su edad"))
+# if edad >= 18: 
+#     print("sos mayor")
+# elif edad < 18:
+#     print("sos menor")
+
+#Ejercicio 2
+#codigo
+#codigo
+saldo = int(input("ingrese el saldo a pagar"))
+if saldo <= 100:
+    print("paga con efectivo")
+elif saldo > 100 and saldo < 300:
+    print("paga con tarjeta de debito")
+else:
+    print("paga con tarjeta de credito")
+
+#Ejercicio 3
+#codigo
+
+#Ejercicio 4
+#codigo
+
+#Ejercicio 5
+#codigo
+
+#Ejercicio 6
+#codigo
+
+#Ejercicio 7
+#codigo
+
+#Ejercicio 8
+#codigo
+
+#Ejercicio 9
+#codigo
+
+#Ejercicio 10
+#codigo

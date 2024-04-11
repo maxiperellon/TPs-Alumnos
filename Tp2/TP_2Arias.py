07c74ab16e0a105c41da8f87cceb442fbf76c972
Botthank <nosequeponeralv202@gmail.com>
2024-04-11 11:44:33 -0300

    TP2_ARIAS

---
+++ Tp2/TP_2Arias.py
@@ -0,0 +1,120 @@
+#Mayor o no
+#edad = int(input("Por favor, introduce tu edad: "))
+#if edad >= 18:
+#    print("Eres mayor de edad.")
+#else:
+#    print("Eres menor de edad.")
+
+#GASTOS
+#x=int(input("saldo a pagar"))
+#if x<=100:
+ #   print("pago en efectivo")
+
+#elif 100<x<300:
+#    print("pago con tarjeta de debito")
+#else:
+#    print("pago con tarjeta de credito")
+
+#DIVIDIR
+#dividiendo=float(input("ingrese el dividiendo"))
+#divisor=float(input("ingrese el divisor"))
+#if divisor == 0:
+ #   print("error no se puede dividir por cero")
+
+#else:
+#    resultado=dividiendo/divisor
+#    print("el resultado de la division es") 
+
+#IMPAR O PAR
+#numero=int(input("ingrese un numero entero"))
+
+#if numero % 2==0:
+#    print(numero,"es un numero par")
+#else:
+#    print(numero,"es un numero impar")
+
+#edad por pago
+
+#edad= int(input("ingrese la edad del cliente"))
+
+#if edad<4:
+#    precio=0
+#elif edad<=18:
+#    precio=200
+#else:
+#    precio=500
+
+#print(" el precio de la entrada es:$"+ str(precio))
+
+
+#negativo o positivo
+
+#numero=input("por favpr ingresa un numero")
+
+#if numero>0:
+ #   print("el numero es positivo")
+#elif numero < 0:
+ #   print("el numero es negativo")
+#else:
+#    print("numero es cero")
+
+#el mayor
+
+#numero1=int(input("ingrese el primer numero"))
+#numero2=int(input("ingrese el segundo numero"))
+#numero3=int(input("ingrese el tercer numero"))
+
+
+#if numero1 >= numero2 and numero1>=numero3:
+ #   mayor=numero1
+#elif numero2>=numero1 and numero2>=numero3:
+ #   mayor=numero1
+#else:
+ #   mayor=numero3
+
+#print("el mayor numero es:",mayor)
+
+#multiplo   
+
+#num1=int(input("ingrese el primer numero"))
+#num2=int(input("ingrese el primer numero"))
+
+#if num1 % num2 ==0:
+#    print (num1 "es multiplo de"+num2)
+#else:
+#    print (num1"no es multiplo de"+num2)
+
+
+#semanas
+
+#if numero == 1:
+#    print("Lunes")
+#elif numero == 2:
+#    print("Martes")
+#elif numero == 3:
+#    print("Miércoles")
+#elif numero == 4:
+#    print("Jueves")
+#elif numero == 5:
+#    print("Viernes")
+#elif numero == 6:
+#    print("Sábado")
+#elif numero == 7:
+#    print("Domingo")
+#else:
+#    print("Número fuera de rango. Por favor ingresa un número del 1 al 7.")
+
+#caracteres
+
+
+#caracter = input("Ingrese un carácter: ")
+
+
+#caracter = caracter.lower()
+
+
+#if caracter in ('a', 'e', 'i', 'o', 'u'):
+ #   print(f"{caracter} es una vocal.")
+#else:
+#    print(f"{caracter} es una consonante.")
+

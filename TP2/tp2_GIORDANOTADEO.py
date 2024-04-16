ec461d4eb29d49422788794ea58c6924008b5429
giordanotade <tadegiordano@gmail.com>
2024-04-16 12:53:21 -0300

    tp2 giordano

---
+++ TP2/tp2_GIORDANOTADEO.py
@@ -0,0 +1,101 @@
+#PROFESOR: MAXIMILIANO PERELLÓN
+#ALUMNO: TADEO GIORDANO
+
+#           TRABAJO PRACTICO Nº2 - CONDICIONALES
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+
+#Ejercicio 1
+#programaUsuario = int(input('Ingrese su edad'))
+#if programaUsuario >= 18:
+#print("Es mayor de edad")
+#else:
+#print("No es mayor de edad")
+
+#Ejercicio 2
+#programa = float(input('Ingrese cuánto quiere gastar'))
+#if programa <= 100:
+#print('El pago será en efectivo')
+#elif programa > 100 and programa < 300:
+#print('El pago será con tarjeta de débito')
+#else:
+#print('El pago será con tarjeta de crédito') 
+
+#Ejercicio 3
+#numero1 = float(input("Ingrese el primer número: "))
+# numero2 = float(input("Ingrese el segundo número: "))
+#if numero2 == 0:
+#print("Error: No se puede dividir por cero")
+#else:
+#resultado = numero1 / numero2
+#print("El resultado de la división es: ", resultado)
+
+#Ejercicio 4
+#numero_usuario = input ("Ingrese un número")
+#if numero_usuario % 2 == 0 :
+#    print (numero_usuario , "es par.")
+#else:
+#    print (numero_usuario , "no es par.")
+
+#Ejercicio 5
+#edadCliente = int(input("¿Cuál es la edad del cliente?"
+#if edadCliente < 4:
+#print("El precio es gratis")
+#elif edadCliente>=4 and edadCliente<=18:
+#print("El precio será de $200")
+#elif edadCliente>18:
+#print("El precio será de $500")
+
+
+#Ejercicio 6
+#numero_usuario = int(input("Ingrese un número"))
+#if numero_usuario = 0:
+#print("El número es cero")
+#elif numero_usuario < 0:
+#print("El número es negativo")
+#else:
+#print("El número es positivo")
+
+
+#Ejercicio 7
+# num1 = int(input("Ingrese un número "))
+# num2 = int(input("Ingrese un segundo número "))
+# num3 = int(input("Ingrese un último número "))
+# numMasGrande = num1
+
+# if num2 > numMasGrande:numMasGrande = num2
+# if num3 > numMasGrande:numMasGrande = num3
+# print("El número entero más grande es:", numMasGrande)
+
+#Ejercicio 8
+#num1 = int(input("Ingrese un número "))
+#num2 = int(input("Ingrese un número "))
+#if num1 % num2 == 0: print("El primer número es multiplo del segundo")
+#else: print("El primer número no es multiplo del segundo")
+
+#Ejercicio 9
+#num_usuario = int(input("Ingrese un número del 1 al 7"))
+#if  (num_usuario==1):
+#print("Lunes")
+#elif (num_usuario==2):
+#print("Martes")
+#elif (num_usuario==3):
+#print("Miércoles")
+#elif (num_usuario==4):
+#print("Jueves")
+#elif (num_usuario==5):
+#print("Viernes")
+#elif (num_usuario==6):
+#print("Sábado")
+#else:
+#print("Domingo")
+
+#Ejercicio 10
+#caracter_usuario = str(input('Ingrese una letra'))
+#if caracter_usuario == 'a' or 'e'  or 'i' or 'o' or 'u':
+#print("Es una vocal")
+#else: print("Es una consonante")

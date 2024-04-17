21f5706f8258341d4ce9ec425d2d926174cbc59c
Botthank <nosequeponeralv202@gmail.com>
2024-04-17 14:10:41 -0300

    TP3_arias

---
+++ TP3/TP_3_Arias.py
@@ -0,0 +1,96 @@
+#imprimir numeros del 1 al 10
+
+#for i in range(1,11):
+ #   print(i)
+
+
+#promedio
+
+#lista_numeros[5,10,15,20,25]
+
+#suma_total = 0
+#for numero in lista_numeros:
+#    suma_total += numero
+
+#promedio= suma_total / len(lista_numeros)
+
+#print("El promedio de la lista de numeros es:",promedio)
+
+
+#imprimir numeros pares
+
+#numero= int(input("por favor ingrese un numero"))
+#contador= 2
+
+#print("numeros pares hasta",numero,":")
+
+#while contador <= numero:
+#    print(contador)
+#    contador+=2
+
+
+
+#letra por letra 
+
+#palabra=input("por favor,ingrese una palabra")
+
+#for letra in palabra
+#print(letra)
+
+#calcular la suma de numeros impares entre 1 y ese numero
+
+
+#numero=int(input("ingrese un numero entero positivo"))
+
+#if numero < 0:
+#    print("por favor ingrese un numero positivo")
+#else:
+ #   for i in range (1,numero+1,2):
+  #      suma +=i 
+   #     print("la suma de los numeros impares entre 1 y"numero "es" suma)
+
+
+#eje 6:suma de numeros pares 
+
+#numero = int(input("Ingrese un número entero positivo: "))
+#suma = 0
+#contador = 2
+
+#while contador <= numero:
+#    suma += contador
+#    contador += 2
+
+#print("La suma de todos los números pares desde 2 hasta", numero, "es:", suma)
+
+
+#Ejer 7:contar la cantidad de numeros pares e impares
+
+#numeros = input("Ingrese una serie de números enteros separados por espacios: ")
+
+#numeros_lista = numeros.split()
+
+#pares = 0
+#impares = 0
+
+
+#for num in numeros_lista:
+#    num = int(num)
+#    if num % 2 == 0:
+#        pares += 1
+#    else:
+#        impares += 1
+
+#print("Cantidad de números pares:", pares)
+#print("Cantidad de números impares:", impares)
+
+#ejer 8
+
+#numero = int(input("Ingrese un número entero: "))
+#contador = 1
+
+#print(f"Tabla de multiplicar del {numero}:")
+#while contador <= 10:
+#    resultado = numero * contador
+#    print(f"{numero} x {contador} = {resultado}")
+#    contador += 1
+

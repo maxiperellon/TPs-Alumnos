4d0a590c1a6830e8f0bfa98e4586fb00cc5f2dcd
Botthank <nosequeponeralv202@gmail.com>
2024-04-23 12:52:36 -0300

    tp3 y repaso

---
+++ Repasos/Repaso bucle and condi.py
@@ -0,0 +1,72 @@
+#imprimir un numero del 1 al 10
+
+#for i in range (1,11):
+#    print(i)
+
+#imprimir del 1 al 20
+
+#for i in range(2,21,2):
+#    print(i)
+
+#positiva , negativo o cero
+
+#numero=float(input("ingrese un numero"))
+
+#if numero>0:
+#    print("el numero es positivo")
+#elif numero<0:
+#    print("el numero es negativo")
+#else:
+#    print("el numero es cero")    
+
+#del 1 al 50 para multiplos de 3
+
+#for i in range(1,51):
+#    if i %3==0 and i %5==0:
+#        print("hola chau")
+#    elif i %3 ==0:
+#        print("hola")
+#    elif i %5==0:
+#        print("chau")
+#    else:
+#        print("este numero no es multiplo",i)            
+
+
+#cadena de usuario y verificar si tiene letra a
+
+#cadena_usuario=input("por favor, ingrese una cadena de texto")
+
+#if "a" in cadena_usuario:
+#    print ("la cadena de texto contiene la letra a")
+#else:
+#    print("la cadena de texto no contiene la letra a")
+
+#contar cantidad de vocales 
+
+#cadena=input("ingrese una cadena de texto")
+
+#contador_vocales=0
+
+#for letra in cadena:
+#    if letra_lower() in "aeiou":
+#        contador_vocales+=1
+
+#print("la cantidad de vocales en la cadena es ",contador_vocales)
+
+
+#verificacion de contraseña
+
+#contraseña="contraseña123"
+
+#while intento>0:
+#    intento=input("ingrese la contraseña ")
+#    if intento==contraseña:
+#        print("contraseña es correcta.Acceso anticipado")
+#        break
+#    else:
+#        intento -=1
+#        if intento==0:
+#            print("has agotado todos tus intentos.acceso denegado")
+#        else:
+#            print("contraseña incorrecta, Te quedan {intentos} intentos")
+

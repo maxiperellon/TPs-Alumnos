027bad125ea8bff0f40fbe7c9b7df1f41ee27386
BigMaxi <maxigomez4425@gmail.com>
2024-04-23 12:09:22 -0300

    a

--- TP3/tp3_APELLIDOALUMNO.py
+++ TP3/tp3_APELLIDOALUMNO.py
@@ -3,23 +3,22 @@
 
 #           TRABAJO PRACTICO Nº3 BUCLES
 
-#PARA COMENTAR UN BLOQUE DE CODIGO: 
+#PARA COMENTAR UN BLOQUE DE CODIGO:
 ##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
 
-#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO:
 ##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
 
 #Ejercicio 1
 # for x in range (1,11):
 #     print(x)
 
+
 # #Ejercicio 2
 # numeros = [1, 2, 3, 4, 5]
 # suma = 0
-
 # for numero in numeros:
 #     suma += numero
-
 # promedio = suma / len(numeros)
 # print("El promedio es:", promedio)
 
@@ -39,10 +38,13 @@
 # #Ejercicio 5
 # numero = int(input("Ingrese un numero entero positivo: "))
 # i = 1
+# suma = 0
 # while i <= numero:
 #     if i % 2 != 0:
-#         print(i)
+#         suma += i
 #     i += 1
+# print("La suma de los números impares es:", suma)
+
 
 # #Ejercicio 6
 # numero = int(input("Ingrese un numero entero positivo: "))
@@ -55,4 +57,24 @@
 
 # #Ejercicio 7
 
+numeros = input("Ingrese una serie de números enteros separados por espacios: ")
+pares = 0
+impares = 0
+numeros2 = numeros.split()
+for num in numeros2:
+    if int(num) % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+print("Cantidad de números pares:", pares)
+print("Cantidad de números impares:", impares)
+
+
 #Ejercicio 8
+numero = int(input("Ingrese un número entero: "))
+i = 1
+print("Tabla de multiplicar del", numero, ":")
+while i <= 10:
+    resultado = numero * i
+    print(numero, "x", i, "=", resultado)
+    i += 1

ff2b3d4b3448db5e868607a44f11883df9a5602a
giordanotade <tadegiordano@gmail.com>
2024-04-22 23:28:11 -0300

    tp3 finalizado

---
+++ TP1/tp1_GIORDANOTADEO.py
@@ -0,0 +1,77 @@
+#PROFESOR: MAXIMILIANO PERELLÓN
+#ALUMNO: TADEO GIORDANO
+
+#           TRABAJO PRACTICO Nº1
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+
+#Ejercicio 1
+#base = float(input("Ingrese la base del triángulo: "))
+#altura = float(input("Ingrese la altura del triángulo: "))
+#area_triangulo = (base * altura) / 2
+#print("El área del triángulo es de ", area_triangulo, "cm²")
+
+#Ejercicio 2
+#nombre = input("Ingrese su nombre completo: ")
+#edad = int(input("¿Cuántos años tienes? "))
+#año_curso = int(input("¿En qué año estás actualmente en la escuela/facultad? "))
+#carrera = int(input("¿De qué carrera o ciclo lectivo estás? "))
+#print("Soy ",  nombre, " tengo ", edad, " estoy en el ", año_curso," en la carrera/ciclo lectivo ", carrera)
+
+#Ejercicio 3
+#numero1 = int(input("Ingrese un número entero: "))
+#numero2 = int(input("Ingrese otro número entero: "))
+#suma =  numero1 + numero2
+#print(suma)
+
+#Ejercicio 4
+#radio = float(input("Ingrese el radio del círculo: "))
+#diametro = radio * 2
+#pi = 3.14
+#area = diametro * pi
+#print(area)
+
+#Ejercicio 5
+#numero1 = int(input("Introduce un valor: "))
+#numero2 = int(input("Introduce otro valor: "))
+#numero3 = int(input("Introduzca un último valor: "))
+#promedio = (numero1 + numero2 + numero3) / 3
+#print("El promedio es: ", promedio)
+
+#Ejercicio 6
+#numero1 = int(input("Introduzca un valor para intercambiar: "))
+#numero2 = int(input("Introduzca otro valor para intercambiar: "))
+#numTemporal = numero1
+#numero1 = numero2
+#numero2 = numTemporal
+#print("Valores intercambiados: ", numero1,"y ", numero2)
+
+#Ejercicio 7
+#numero1 =  int(input("Introduzca un número entero: "))
+#numero2 =  int(input("Introduzca otro numero entero: "))
+#suma =  numero1 + numero2
+#resta= numero1 - numero2
+#producto = numero1 * numero2
+#division = numero1 / numero2
+#print("La suma de los números es:", suma, "\nLa resta de los números es:", resta, "\nEl producto de los números es:", producto, "\nLa división de los números es:", division)
+
+#Ejercicio 8
+# listaMaterias =  []
+# materia = ""
+# while materia != "fin":
+#    materia = str(input("Ingrese las materias que desea agregar a la lista y escriba 'fin' para terminar: "))
+#    if materia == "fin":
+#     print("Materias del curso: ", listaMaterias)
+#     break
+#    listaMaterias.append(materia)
+
+#Ejercicio 9
+#listaGaseosas = ["coca cola", "fanta", "sprite", "manaos", "pepsi", "paso de los toros", "mirinda", "goliat sport"]
+#print(listaGaseosas)
+#usuario_gaseosas = int(input("Ingrese qué gaseosa desea ver por medio de números enteros: "))
+#elemento = listaGaseosas[usuario_gaseosas]
+#print("La información de la gaseosa que el usuario quiere ver es: ", elemento)

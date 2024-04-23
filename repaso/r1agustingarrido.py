0e0f2335c3bfd75798c3830963fd7f2bf28a83b5
destrozawelas <agusgarridococo@gmail.com>
2024-04-23 12:36:18 -0300

    Create r1agustingarrido.py

---
+++ repaso/r1agustingarrido.py
@@ -0,0 +1,87 @@
+#PROFESOR: MAXIMILIANO PERELLON
+#ALUMNO: agustin garrido
+
+#           REPASO Nº1 CONDICIONALES - BUCLES
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K / CTRL + U
+
+#Ejercicio 1
+#codigo1
+
+#for i in range(1, 11):
+#    print(i)
+
+
+#Ejercicio 2
+#codigo
+#for i in range(2, 21, 2):
+#    print(i)
+
+#Ejercicio 3
+#codigo
+
+#numero = float(input("Por favor, ingresa un número: "))
+
+#if numero > 0:
+#    print("El número es positivo.")
+#elif numero < 0:
+#    print("El número es negativo.")
+#else:
+#    print("El número es cero.")
+
+
+#Ejercicio 4
+#codigo
+
+#numero = float(input("Por favor, ingresa un número: "))
+
+#if numero > 0:
+#    print("El número es positivo.")
+#elif numero < 0:
+#    print("El número es negativo.")
+#else:
+#    print("El número es cero.")
+
+#Ejercicio 5
+#codigo
+
+#cadena = input("Por favor, ingresa una cadena de texto: ")
+
+#if "a" in cadena:
+#    print("La cadena de texto contiene la letra 'a'.")
+#else:
+#    print("La cadena de texto no contiene la letra 'a'.")
+
+#Ejercicio 6
+#codigo
+
+#cadena = input("Por favor, ingresa una cadena de texto: ")
+
+#vocales = 0
+#for letra in cadena:
+#    if letra in "aeiouAEIOU":
+#        vocales += 1
+
+#print("La cadena de texto contiene", vocales, "vocales.")
+
+#Ejercicio 7
+#codigo
+# Programa que verifica una contraseña con un máximo de 3 intentos
+#contraseña_correcta = "contraseña"  # Esta es la contraseña correcta
+#intentos = 0
+
+#while intentos < 3:
+#    contraseña = input("Por favor, ingresa tu contraseña: ")
+#    if contraseña == contraseña_correcta:
+#        print("¡Contraseña correcta!")
+#        break
+#    else:
+#        print("Contraseña incorrecta. Inténtalo de nuevo.")
+#        intentos += 1
+
+#if intentos == 3:
+

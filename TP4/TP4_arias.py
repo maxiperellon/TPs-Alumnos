bebdce0df8a01544fa3cc3104292f4bebffc6b9f
Botthank <nosequeponeralv202@gmail.com>
2024-05-08 13:15:17 -0300

    tp4_arias

---
+++ TP4/TP4_arias.py
@@ -0,0 +1,87 @@
+# ejercicio 1:crea una funcion que reciba como parametros dos numeros y devuelva la suma  de ambos 
+
+#def suma(num,num2):
+#    return num +num2
+
+#resultado= suma(3,5)
+#print (resultado)
+
+#ejercicio 2
+
+#def suma_lista(lista):
+ #   suma=sum(lista)
+  #  return suma
+    
+    
+
+
+#ejercicio 3 crea una funcion que reciba parametro una lista y devyelva la suma los elemntos ordenados de forma ascendente
+
+#def suma_lista(lista):
+#    suma=sum(lista)
+#    return suma
+ 
+#mi_lista= [1,2,3,4,5]
+#resultado=suma_lista(mi lista)
+#print("la suma de los elementos de la lista es" resultado)
+
+#ejercicio 4
+
+#def contar_vocales(cadena):
+#    vocales="aeiouAeiou"
+#    contador=0
+#    for caracter in cadena:
+#        if caracter in vocales:
+#            contador +=1
+#    return contador
+
+#ejercicio 5:
+
+#def numero_mas_grande(lista):
+#    if not lista:
+#        return None
+#    else:
+
+#        return max(lista)
+
+#lista que hice de ejemplo:
+#umeros=[10,5,20,8,15]
+#print("el numero mas grande de la lista es ")
+#numero_mas_grande(numeros)
+
+#ejercicio 6
+
+#def numeros_pares(lista):
+#    pares=[num for num in lista if num %2 == 0]
+#    return pares       
+
+#numeros=[1,2,3,4,5,6,7,8,9,10]
+#pares=numeros_pares(numeros)
+#print(pares)
+
+#ejercicio 7
+
+#def buscar_numero(lista,numero)
+#    return numero in lista 
+
+#mi_lista=[1,3,5,7,9]
+
+#print(buscar_numero(mi_lista,5))
+#print(buscar_numero(mi_lista,2))
+
+#ejercicio 8
+
+#def longitud_cadena(cadena)
+#    return len(cadena)
+
+#ejercicio 9
+#import math
+
+#def area_circulo(radio):
+#    return math.pi*radio**2
+
+#def area_triangulo(base,altura):
+#    return (base* altura)/2
+
+#def area_cuadrado(lado):
+#    return lado **2

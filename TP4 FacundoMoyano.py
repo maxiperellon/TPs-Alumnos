6b668e72b2162b3559bb0d46dc11b336bc4919ab
FacuLOLOKY <144150033+FacuLOLOKY@users.noreply.github.com>
2024-05-10 18:05:44 -0300

    Add files via upload

---
+++ TP4 FacundoMoyano.py
@@ -0,0 +1,90 @@
+#PROFESOR: MAXIMILIANO PERELLON
+#ALUMNO: ....
+
+#           TRABAJO PRACTICO Nº4 FUNCIONES
+
+#PARA COMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES COMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K | CTRL + C
+
+#PARA DESCOMENTAR UN BLOQUE DE CODIGO: 
+##SELECCIONAR EL CODIGO QUE QUERES DESCOMENTAR E INGRESAR LOS ATAJOS DE TECLADO: CTRL + K | CTRL + U
+
+#Ejercicio 1
+#codigo
+
+""" 
+def suma (number1,number2 ):      
+   sum = number1 + number2
+   return sum
+
+print(suma(5,4))
+ """
+
+#Ejercicio 2
+#codigo
+
+""" lista =  (1, 2, 3, 4)
+def lista(lista):
+    for y in lista: 
+
+       print (lista(1, 2, 3, 4)) """
+
+#Ejercicio 3
+#codigo
+
+""" def ordenar_lista(lista):
+    return sorted(lista)
+_listashex = [5, 3, 9, 1]
+lista_ordenada = ordenar_lista(_listashex)
+print(lista_ordenada)  # Imprime: [1, 3, 5, 9]
+ """
+#Ejercicio 4
+#codigo
+
+""" def contar_vocales(cadena):
+    vocales = 'aeiouAEIOU'
+    return sum(1 for letra in cadena if letra in vocales)
+cadenagod = "Milanesas de pollo"
+cantidad_vocales = contar_vocales(cadenagod)
+print(cantidad_vocales)  # Imprime: 4 """
+
+#Ejercicio 5
+#codigo
+
+""" def maximo(lista):
+    return max(lista)
+milista = [5, 3, 1453209, 1]
+maximo_numero = maximo(milista)
+print(maximo_numero)  # Imprime: 9 """
+
+#Ejercicio 6
+#codigo
+
+""" def obtener_pares(lista):
+    return [num for num in lista if num % 2 == 0]
+milista = [5, 3, 9, 1, 2, 8, 6]
+lista_pares = obtener_pares(milista)
+print(lista_pares)  # Imprime: [2, 8, 6] """
+
+#Ejercicio 7
+#codigo
+
+""" def esta_en_lista(lista, numero):
+    return numero in lista
+lista = [5, 3, 9, 1, 2, 8, 6]
+numero = 4
+esta = esta_en_lista(lista, numero)
+print(esta)  # Imprime: True """
+
+#Ejercicio 8
+#codigo
+
+def longitud_cadena(cadena):
+    return len(cadena)
+cadena = "Milanga de pollo cronica"
+longitud = longitud_cadena(cadena)
+print(longitud)  # Imprime: 10
+
+#Ejercicio 9
+#codigo
+
